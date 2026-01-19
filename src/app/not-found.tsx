c874fa33857c6f05c757147534f3349bb363c5e0
Linda Giorgi <giorgi.linda@gmail.com>
2026-01-19 13:26:04 +0100

    fix issue with not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import styles from "./not-found.module.css";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>404</h1>
       <p className={styles.description}>Page not found</p>
-      <a href="/" className={styles.link}>
+      <Link href="/" className={styles.link}>
         Go back home
-      </a>
+      </Link>
     </div>
   );
 }
